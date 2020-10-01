6a5af4565fe9c98b0a84dd221a6eb156268de074
TheAstutian <speakingatoms@gmail.com>
2020-10-01 21:11:03 +0100

    app.js updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import Header from './components/header/header.component'; 
+
 import Navigation from './components/navigation/navigation.component';
 function App() {
   return (
     <div className="App">
       <Navigation />
+      
      <HomePage />
     </div>
   );
