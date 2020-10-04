628c8ee00157c2c4284febfc0bfe187e79e95dd9
TheAstutian <speakingatoms@gmail.com>
2020-10-04 10:42:01 +0100

    added react-router-dom functionality to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
 import Navigation from './components/navigation/navigation.component';
+import Header from './components/header/header.component';
+
+
 function App() {
   return (
+
     <div className="App">
       <Navigation />
-      
-     <HomePage />
+
+     <Switch>
+       <Route exact path='/' component={HomePage} />
+       <Route exact path="/you" component={Header} />
+     </Switch>
     </div>
   );
 }
