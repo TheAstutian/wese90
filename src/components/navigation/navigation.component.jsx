0d097e09d622b5b2b7c58bac66db2037e375df90
TheAstutian <speakingatoms@gmail.com>
2020-11-02 20:30:23 +0100

    styling navigation.component

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import Button from '../button/button.component';
+import './navigation.style.css';
 
 const Navigation = () => (
-    <div>
-        <div>
+    <div className='container'>
+        <div className='title-container'>
             <h1>Wese90</h1>
         </div>
         
-        <div>
+        <div className='nav-menus'>
             <div><p>How it works</p></div>
             <div> <Button>Contact Us</Button></div>
         </div>
