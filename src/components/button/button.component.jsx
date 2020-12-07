b1193faf4539c46f06e73fa87bbfbc4b701fd8f6
TheAstutian <speakingatoms@gmail.com>
2020-12-07 16:57:30 +0100

    styling header component - added picture, styled text area

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react'; 
+import './button.style.scss';
 
 const Button = ({children, ...otherProps}) =>(
     <div>
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,19 +9,19 @@ const Header = () => (
       
         <div className="banner-image"
          style ={{
-          backgroundImage: `url(${'https://cdn.pixabay.com/photo/2020/11/08/15/23/phone-5724015_1280.jpg'})`
+          backgroundImage: `url(${'https://images.unsplash.com/photo-1569040029205-a03a8b455808'})`
           }} 
           />
         
         <div className='header-content'>
               <div className='header-title'>
-            <span> Your </span>
-            <span> Local Man</span>
-            </div>
-            <div>
-            <span>Delivering smart solutions to your tech problems...</span>
-            <Button>Learn More</Button>
-            </div>
+              <span> Your </span>
+              <span> Local Man</span>
+              </div>
+              <div className='header-txtbtn'>
+              <span>Delivering smart solutions to</span><span> your tech problems...</span>
+              <Button>Learn More</Button>
+              </div>
         </div>
       
       
