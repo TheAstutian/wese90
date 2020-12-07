8e9f997e6f326b1f8f8aaaaadbb23da26dfe04ad
TheAstutian <speakingatoms@gmail.com>
2020-12-07 14:03:12 +0100

    added background image

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,16 +6,25 @@ import './header.style.scss';
 const Header = () => (
     <div className='header-container'>
         
-      <div>
-        <div className="banner-image"/>
-      <div className='header-title'>
-      <span> Your </span><span> Local Man</span>
-      </div>
-      <div>
-      <span>Delivering smart solutions to your tech problems...</span>
-      <Button>Learn More</Button>
-      </div>
-      </div>
+      
+        <div className="banner-image"
+         style ={{
+          backgroundImage: `url(${'https://cdn.pixabay.com/photo/2020/11/08/15/23/phone-5724015_1280.jpg'})`
+          }} 
+          />
+        
+        <div className='header-content'>
+              <div className='header-title'>
+            <span> Your </span>
+            <span> Local Man</span>
+            </div>
+            <div>
+            <span>Delivering smart solutions to your tech problems...</span>
+            <Button>Learn More</Button>
+            </div>
+        </div>
+      
+      
     </div>
 )
 
