0f4d8e1a4ef34c08dd09f2e723c7b593352fc823
TheAstutian <speakingatoms@gmail.com>
2020-12-31 20:30:02 +0100

    added content to about us

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -5,9 +5,11 @@ const About = () =>(
     <div className='about-container'>
         <span>About Us</span>
         <div className='about-content'>
-            <p>We are a group of talented individuals who are motivated and dedicated to solving problems in different forms, shapes and sizes. </p>
-            <p>Each week, we send high-quality, perfectly proportioned ingredients to customers nationwide to prepare delicious, wholesome meals at home. We introduce our community of home cooks to new ingredients, flavors, and techniques with seasonally inspired recipes that are delicious, fun, and easy to prepare.</p>
+            <p>We are a group of talented individuals who are motivated and dedicated to solving problems in different forms, shapes and sizes. Each day, we meet our customers at the point of their need.</p>
+            
+            <p>We believe that for all businesses in this globalised economy in which we live, the supply chain is a key enabler of successful business strategy. An efficient and effective supply chain now needs to be cognisant of issues affecting supply and demand all over the world. We therefore seek to design, plan, implement and manage cutting-edge supply chain solutions for our clients that enable them become more competitive, leaner, and more effective as an organisation.</p>
 
+            <p> We specialize in crafting efficient and cost effective logistic solutions customized to client's specific needs. This allows you to focus on core competencies and receive a logistical advantage over the competition through the use of our third party logistics services.</p>
         </div>
     </div>
 )
