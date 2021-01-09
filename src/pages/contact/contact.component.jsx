2c3e457d53cf20e1a5c4dbcd483f1fca776df2b3
TheAstutian <speakingatoms@gmail.com>
2021-01-09 19:50:24 +0100

    added contact form to contactus

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -4,6 +4,24 @@ const ContactUs = () => (
     <div>
         <h1>Contact Page</h1>
 
+        <div>
+                <form className="contact-form" onSubmit={ (e) => this.formSubmit(e)}>
+                        <label class="message" htmlFor="message-input">Your Message</label>
+                        <textarea onChange={e => this.setState({ message: e.target.value})} name="message" class="message-input" type="text" placeholder="Please write your message here" value={this.state.message} required/>
+
+                        <label class="message-name" htmlFor="message-name">Your Name</label>
+                        <input onChange={e => this.setState({ name: e.target.value})} name="name" class="message-name" type="text" placeholder="Your Name" value={this.state.name}/>
+
+                        <label class="message-email" htmlFor="message-email">Your Email</label>
+                        <input onChange={(e) => this.setState({ email: e.target.value})} name="email" class="message-email" type="email" placeholder="your@email.com" required value={this.state.email} />
+
+                        <div className="button--container">
+                            <button type="submit" className="button button-primary">{ this.state.buttonText }</button>
+                        </div>
+                </form>
+
+        </div>
+
         <span>Doing business in Nigeria is not easy. The system is rusty and there’s a ton of obstacles to overcome before you can get things done. To be a successful entrepreneur, you need to be smart and more importantly, have the right connections. 
 
 This is where Visita xxx comes in. As an xxx dealhunter/ seller or retailer based inside or outside of Lagos, you need a safe plug that is familiar with the terrain to handle your items. 
