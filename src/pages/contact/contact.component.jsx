6c1c0bc0844b1837898bc257164d4bc5d82bc518
TheAstutian <speakingatoms@gmail.com>
2021-01-24 09:42:16 +0100

    setting up contactUs with axios

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, {useState} from 'react';
 
 
@@ -12,6 +13,47 @@ const ContactUs = () => {
         })
     }
 
+    
+    const formSubmit = (e) => {
+        e.preventDefault();
+
+        setData({
+            ...data,
+            buttonText: 'Sending...'
+        })
+
+        axios.post('/api/sendmail', data)
+        .then( res => {
+            if (res.data.result !== 'success'){
+                setData({
+                    ...data,
+                    buttonText: 'Failed to send',
+                    sent: false,
+                    err: 'fail'
+                })
+                setTimeout (()=> {
+                    resetForm()
+                }, 6000)
+            } else {
+                setData({
+                    ...data,
+                    sent: true,
+                    buttonText: 'Sent',
+                    err: 'success'
+                })
+                setTimeout(()=>{
+                    resetForm();
+                }, 6000)
+            }
+
+        }).catch( (err) => {
+            setData({
+                ...data,
+                buttonText: 'Failed to send',
+                err: 'fail'
+            })
+        })
+    }
 
     return (
         <div>
@@ -20,9 +62,9 @@ const ContactUs = () => {
         <div>
                 <form className="contact-form" >
                         
-                        <input required id='full-name' name="name" value={data.name} onChange={handleChange} type="text" placeholder="Full name" />
+                        <input required id='full-name' name="name" value={data.name} onChange={handleChange} type="text" placeholder="your full name" />
 
-                        <input required id='email' name="email"  type="email" placeholder="your@email.com" value={data.email} onChange={handleChange}  />
+                        <input required id='email' name="email"  type="email" placeholder="your email address" value={data.email} onChange={handleChange}  />
                         <textarea required name="message" class="message-input" type="text" placeholder="Please write your message here" value={data.message} onChange={handleChange}/>
 
                         <div className="button--container">
