e5ce03def81ab9be606f94ebd6bd2a92e3bf34b3
TheAstutian <speakingatoms@gmail.com>
2021-01-31 22:43:52 +0100

    style for ads page

--- src/components/adverts/adverts.component.jsx
+++ src/components/adverts/adverts.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './adverts.style.scss';
 
 const Adverts = () =>(
-    <div>
+    <div className='ad-container'>
         <h1>Adverts</h1>
         <span>We have a teeming audience of gadget enthusiats who are always looking for good deals that you can sell your goods to. We've got wholesalers, retailers and end users who want to buy tech gadgets from you.</span>
-        <div>
+        <div className='the-images'>
         <img src="https://images.unsplash.com/photo-1609692814858-f7cd2f0afa4f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8YWlycG9kcyUyMHByb3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" />
         <img src="https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" />
         <img src="https://images.unsplash.com/photo-1607853202273-797f1c22a38e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGxheXN0YXRpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" />
