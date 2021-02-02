97366f3777ea2a8d2d804e9cc9f8e159a3d4ceca
TheAstutian <speakingatoms@gmail.com>
2021-02-02 12:52:06 +0100

    building adverts page. added more content

--- src/components/adverts/adverts.component.jsx
+++ src/components/adverts/adverts.component.jsx
@@ -5,13 +5,34 @@ const Adverts = () =>(
     <div className='ad-container'>
         <h1>Adverts</h1>
         <span>We have a teeming audience of gadget enthusiats who are always looking for good deals that you can sell your goods to. We've got wholesalers, retailers and end users who want to buy tech gadgets from you.</span>
+        <span>And they're interested in what sells: phones, airpods, Apple watches, ps4s, and other gadgets. </span>
         <div className='the-images'>
         <img src="https://images.unsplash.com/photo-1609692814858-f7cd2f0afa4f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8YWlycG9kcyUyMHByb3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" />
         <img src="https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" />
         <img src="https://images.unsplash.com/photo-1607853202273-797f1c22a38e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGxheXN0YXRpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" />
+        <img src="https://images.unsplash.com/photo-1609692814858-f7cd2f0afa4f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8YWlycG9kcyUyMHByb3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" />
+        <img src="https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" />
+        <img src="https://images.unsplash.com/photo-1607853202273-797f1c22a38e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGxheXN0YXRpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" />
         </div>        
-        <span>It doesn't matter if your goods isn't an iPhone, we've got the audience for you. We help sell all sorts of electronics - Apple watches, airpods, macbooks, Playstation 5, etc</span>
-        <span>To get involved, add +23480234dummynumber on Whatsapp. Let's talk </span>
+        <span>It doesn't matter if what you have isn't any of these, your customers are part of our audience. </span>
+
+        <div>
+            Available stuff for sale
+
+            <div>Pic/ description and price</div>
+            <div>Pic/price</div>
+            <div>Pic/price</div>
+            <div>Pic/price</div>
+            <div>Pic/price</div>
+            <div>Pic/price</div>
+            <div>Pic/price</div>
+            <div>Pic/price</div>
+            <div>Pic/price</div>
+
+        </div>
+
+        <h2>How can you get invovled?</h2>
+        <span>Simple contact us via the homepage, or hit up  +23480234dummynumber on Whatsapp. Let's talk </span>
     </div>
 )
 
