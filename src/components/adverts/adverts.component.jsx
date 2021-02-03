c9b0af80294fc0f8aafa84957710273fae16ca15
TheAstutian <speakingatoms@gmail.com>
2021-02-03 20:33:58 +0100

    added more images to ad section

--- src/components/adverts/adverts.component.jsx
+++ src/components/adverts/adverts.component.jsx
@@ -7,27 +7,32 @@ const Adverts = () =>(
         <span>We have a teeming audience of gadget enthusiats who are always looking for good deals that you can sell your goods to. We've got wholesalers, retailers and end users who want to buy tech gadgets from you.</span>
         <span>And they're interested in what sells: phones, airpods, Apple watches, ps4s, and other gadgets. </span>
         <div className='the-images'>
-        <img src="https://images.unsplash.com/photo-1609692814858-f7cd2f0afa4f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8YWlycG9kcyUyMHByb3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" />
-        <img src="https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" />
-        <img src="https://images.unsplash.com/photo-1607853202273-797f1c22a38e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGxheXN0YXRpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" />
-        <img src="https://images.unsplash.com/photo-1609692814858-f7cd2f0afa4f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8YWlycG9kcyUyMHByb3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" />
-        <img src="https://images.unsplash.com/photo-1541807084-5c52b6b3adef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" />
-        <img src="https://images.unsplash.com/photo-1607853202273-797f1c22a38e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGxheXN0YXRpb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" />
+        <img src="https://images.unsplash.com/photo-1595392029711-8a206145f976?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" />
+        <img src="https://images.unsplash.com/photo-1604671368394-2240d0b1bb6c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80" />
+        <img src="https://images.unsplash.com/photo-1600183042195-fa3f15d427e9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80" />
+        <img src="https://images.unsplash.com/photo-1579586337278-3befd40fd17a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80" />
+        <img src="https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80" />
+        <img src="https://images.unsplash.com/photo-1476170434383-88b137e598bb?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80" />
+        <img src="https://images.unsplash.com/photo-1606144042614-b2417e99c4e3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+        <img src="https://images.unsplash.com/photo-1608043152269-423dbba4e7e1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80" />
+        <img src="https://images.unsplash.com/photo-1468495244123-6c6c332eeece?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=622&q=80" />
         </div>        
         <span>It doesn't matter if what you have isn't any of these, your customers are part of our audience. </span>
 
-        <div>
-            Available stuff for sale
 
-            <div>Pic/ description and price</div>
-            <div>Pic/price</div>
-            <div>Pic/price</div>
-            <div>Pic/price</div>
-            <div>Pic/price</div>
-            <div>Pic/price</div>
-            <div>Pic/price</div>
-            <div>Pic/price</div>
-            <div>Pic/price</div>
+        <h2>Available stuff for sale</h2>
+        <div className='ad-items'>
+            
+
+            <div className='item'>Pic/ description and price</div>
+            <div className='item'>Pic/price</div>
+            <div className='item'>Pic/price</div>
+            <div className='item'>Pic/price</div>
+            <div className='item'>Pic/price</div>
+            <div className='item'>Pic/price</div>
+            <div className='item'>Pic/price</div>
+            <div className='item'> Pic/price</div>
+            <div className='item'>Pic/price</div>
 
         </div>
 
