518ff5b6e31ab2913b347f3de8051fa9725fb956
TheAstutian <speakingatoms@gmail.com>
2021-02-04 17:47:14 +0100

    styling for ad page

--- src/components/adverts/adverts.component.jsx
+++ src/components/adverts/adverts.component.jsx
@@ -16,14 +16,26 @@ const Adverts = () =>(
         <img src="https://images.unsplash.com/photo-1606144042614-b2417e99c4e3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
         <img src="https://images.unsplash.com/photo-1608043152269-423dbba4e7e1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80" />
         <img src="https://images.unsplash.com/photo-1468495244123-6c6c332eeece?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=622&q=80" />
+        
         </div>        
         <span>It doesn't matter if what you have isn't any of these, your customers are part of our audience. </span>
 
 
-        <h2>Available stuff for sale</h2>
+        <h2>Available items for sale</h2>
         <div className='ad-items'>
             
 
+            <div className='item'>Pic/ description and price</div>
+            <div className='item'>Pic/ description and price</div>
+            <div className='item'>Pic/ description and price</div>
+            <div className='item'>Pic/ description and price</div>
+            <div className='item'>Pic/ description and price</div>
+            <div className='item'>Pic/ description and price</div>
+            <div className='item'>Pic/ description and price</div>
+            <div className='item'>Pic/ description and price</div>
+            <div className='item'>Pic/ description and price</div>
+            <div className='item'>Pic/ description and price</div>
+            <div className='item'>Pic/ description and price</div>
             <div className='item'>Pic/ description and price</div>
             <div className='item'>Pic/price</div>
             <div className='item'>Pic/price</div>
@@ -36,8 +48,8 @@ const Adverts = () =>(
 
         </div>
 
-        <h2>How can you get invovled?</h2>
-        <span>Simple contact us via the homepage, or hit up  +23480234dummynumber on Whatsapp. Let's talk </span>
+        <h3>How can you get invovled?</h3>
+        <span className='last-span'>Simply contact us via Whatsapp. Let's talk </span>
     </div>
 )
 
