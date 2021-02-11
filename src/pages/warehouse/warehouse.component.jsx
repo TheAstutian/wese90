d992e8449cd2ec3053283d8e5402efc88b370b53
TheAstutian <speakingatoms@gmail.com>
2021-02-11 17:25:39 +0100

    optimized warehouse page for mobile

--- src/pages/warehouse/warehouse.component.jsx
+++ src/pages/warehouse/warehouse.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './warehouse.style.scss'
 
-const WareHouse = () => (
+const WareHouse = () => ( 
     <div className='div'>
         <h1>Worldclass Warehousing</h1>
         <div className='warehouse-content'>
             
         <span>Doing business in Nigeria is not easy. The system is rusty and there’s a ton of obstacles to overcome before you can get things done. To be a successful entrepreneur, you need to be smart and more importantly, have the right connections.  </span>
-        <span>This is where we come in. As an xxx dealhunter/ seller or retailer based inside or outside of Lagos, you need a safe plug that is familiar with the terrain to handle your items. </span>
+        <span>This is where we come in. As a dealhunter/ seller or retailer based inside or outside of Lagos, you need a safe plug that is familiar with the terrain to handle your items. </span>
         
-        <img alt='' src='https://images.unsplash.com/photo-1590247813693-5541d1c609fd' />
+        <img alt='' src='https://images.unsplash.com/photo-1590247813693-5541d1c609fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1084&q=80' />
         <span>What we do is provide an ultra safe environment for your clients to come observe or pick up their packages. We cater to entities based in Lagos but our focus is on dealhunters outside Lagos. </span>
         <span>You can have your items delivered to our home/office address and have items dispatched to clients/customers directly from there. Or your clients can come around in person to pick up their items. We are pretty flexible like that. </span>
 
