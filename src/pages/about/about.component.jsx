078996088a9c07154c5294d90f4aeb02dfa121cf
TheAstutian <speakingatoms@gmail.com>
2021-02-11 18:59:56 +0100

    optimized homepage for faster loading

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -6,24 +6,8 @@ const About = () =>(
         <span>About Us</span>
         <div className='about-content'>
             <p>We are a group of talented individuals who are motivated and dedicated to solving problems in different forms, shapes and sizes. Each day, we meet our customers at the point of their need.</p>
-            
-            <p>We believe that for all businesses in this globalised economy in which we live, the supply chain is a key enabler of successful business strategy. An efficient and effective supply chain now needs to be cognisant of issues affecting supply and demand all over the world. We therefore seek to design, plan, implement and manage cutting-edge supply chain solutions for our clients that enable them become more competitive, leaner, and more effective as an organisation.</p>
-
             <p> We specialize in crafting efficient and cost effective logistic solutions customized to client's specific needs. This allows you to focus on core competencies and receive a logistical advantage over the competition through the use of our third party logistics services.</p>
-            <p>Doing business in Nigeria is not easy. The system is rusty and there’s a ton of obstacles to overcome before you can get things done. To be a successful entrepreneur, you need to be smart and more importantly, have the right connections. 
-
-This is where Visita xxx comes in. As an xxx dealhunter/ seller or retailer based inside or outside of Lagos, you need a safe plug that is familiar with the terrain to handle your items. 
-
-What we do is provide an ultra safe environment for your clients to come observe or pick up their packages. We cater to entities based in Lagos but our focus is on dealhunters outside Lagos. 
-
-You can have your items delivered to our home/office address and have items dispatched to clients/customers directly from there. Or your clients can come around in person to pick up their items. We are pretty flexible like that. 
-
-You'll also have the opportunity to advertise goods/items you have with us on this site. 
-You can find us at computer village Ikeja, while home address is at alimosho
-
-As a bonus, we run "FREE" escrow services. If you are having doubts about doing business with an individual, we can step in and smoothen things over. 
-
-Let us help you solve your problems. We are only a call/text away.</p>
+            <p>Add Google Maps and Contact address</p>
         </div>
     </div>
 )
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -43,12 +43,12 @@ const HomePage = () => (
 
     <div className='second ash'>
         <div className='img'>
-            <img alt='2' src='https://images.unsplash.com/photo-1588130721958-d1392d36ed94' />
+            <img alt='2' src='https://images.unsplash.com/photo-1588130721958-d1392d36ed94?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80' />
         </div>
         <div className='txt'>
             <h2> Dependable Escrow</h2>
             <p> Facilitate your next business deal with our escrow service. It is tested and trusted, and it won't cost you a dime. </p>
-            <a href="https://wa.link/no3b4t"><button>Do Deals</button></a>
+            <a href="https://wa.link/0ia8aw"><button>Do Deals</button></a>
         </div>
 
     </div>
@@ -63,7 +63,7 @@ const HomePage = () => (
         </div>
 
         <div className='img'>
-            <img alt='3' src='https://images.unsplash.com/photo-1548695607-9c73430ba065' />
+            <img alt='3' src='https://images.unsplash.com/photo-1548695607-9c73430ba065?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1184&q=80' />
         </div>
 
     </div>
@@ -90,20 +90,20 @@ const HomePage = () => (
         <h1> What Our Customers Say</h1>
         <div className='testimonial-container'>
             <div className='testimony'>
-            <img alt='6' src='https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg' />
+            <img alt='6' src='https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80' />
             <a href="https://www.nairaland.com/5266068/thread-only-aliexpress-shopper/258#93036833"> <span>"Smooth and seamless transaction with a nairalander and Wese90 as Escrow.
                 Wese90 is highly recommended as your escrow...I give him a 10/10 rating." </span></a>
             </div>
             <div className='testimony'>
-                <img alt='7' src='https://actnow.training/wp-content/uploads/2020/03/8e798becf1500cddf92f7f2ec9be2b8b.jpg' />
+                <img alt='7' src='https://images.unsplash.com/photo-1561677843-39dee7a319ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80' />
             <a href="https://www.nairaland.com/6065269/ps4-sale....sold#93019795"><span>"Just in case you're still doubting him, kindly visit the technology thread and ask about escrow services @wese90. Nah only God fit reward that guy."</span></a>
             </div>
             <div className='testimony'>
-                <img alt='9' src='https://ivy-school.thimpress.com/demo-3/wp-content/uploads/learn-press-profile/5/2448c53ace919662a2b977d2be3a47c5.jpg' />
+                <img alt='9' src='https://images.unsplash.com/photo-1507152832244-10d45c7eda57?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1934&q=80' />
             <a href="https://www.nairaland.com/5032880/all-offerup-letgo-craigslist-5miles/2834#92975096"><span>"@wese90, God bless you everyday for your selfless service. Its highly appreciated"</span></a>
             </div>
             <div className='testimony'>
-                <img alt='8' src='https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg' />
+                <img alt='8' src='https://images.unsplash.com/photo-1509325067210-e6b3643e817a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80' />
             <a href="https://www.nairaland.com/5032880/all-offerup-letgo-craigslist-5miles/2808#92878686"><span>"I just completed a transaction with wizzy89. Facilitated by our able indaboski, president Wese90 as escrow. Transaction was very smooth. I appreciate you boss."</span></a>
             </div>
         </div>
