73d57eb01624425c3432b5a5d7f850c5f6eb8086
TheAstutian <speakingatoms@gmail.com>
2021-02-12 16:49:46 +0100

    troubleshooting bugs- added alt property to iframe element so that deploy on netlify can be successful

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -8,7 +8,7 @@ const About = () =>(
             <p>We are a group of talented individuals who are motivated and dedicated to solving problems in different forms, shapes and sizes. Each day, we meet our customers at the point of their need.</p>
             <p> We specialize in crafting efficient and cost effective logistic solutions customized to client's specific needs. This allows you to focus on core competencies and receive a logistical advantage over the competition through the use of our third party logistics services.</p>
             <div className="maps">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.4026808209146!2d3.338572014504659!3d6.596772995229358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b92270c000113%3A0x8082160dd280eab4!2sComputer%20Village!5e0!3m2!1sen!2sng!4v1613136313201!5m2!1sen!2sng" width="100%" height="300" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.4026808209146!2d3.338572014504659!3d6.596772995229358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b92270c000113%3A0x8082160dd280eab4!2sComputer%20Village!5e0!3m2!1sen!2sng!4v1613136313201!5m2!1sen!2sng" width="100%" height="300" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0" title='iframe'></iframe>
             </div>
             <div className='contacts'>
             <div className='contact'>
